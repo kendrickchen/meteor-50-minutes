99dd9a287f37141a0de6e0cb1c8e79f05378831a
Kendrick Chen <chenkk@hawaii.edu>
2016-10-09 21:35:47 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Created by kenchen on 10/9/16.
+ */
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
